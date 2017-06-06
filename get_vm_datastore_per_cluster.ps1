2de15df3e70d8087313f56af10f5ef31dec8fb05
adingman <adingman@steelcase.com>
2017-06-06 16:50:15 -0400

    Add files via upload

---
+++ get_vm_datastore_per_cluster.ps1
@@ -0,0 +1,6 @@
+﻿$Cluster = Read-Host -Prompt 'Input the name of the Cluster'
+$FileLocation = "C:\temp\VM_CLuster_Host_Datastore_GHQ.csv"
+
+Get-VM -Location $Cluster | Select Name, @{N="Cluster";E={Get-Cluster -VM $_}}, `
+@{N="ESX Host";E={Get-VMHost -VM $_}}, `
+@{N="Datastore";E={Get-Datastore -VM $_}} | Sort Name | Export-Csv -NoTypeInformation $FileLocation
