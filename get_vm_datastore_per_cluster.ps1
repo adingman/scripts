5336c77d782a53ee9c406ead9a3f91e4b8939861
adingman <adingman@steelcase.com>
2017-06-06 17:04:41 -0400

    Change Cluster Variable

--- get_vm_datastore_per_cluster.ps1
+++ get_vm_datastore_per_cluster.ps1
@@ -1,6 +1,6 @@
-﻿$Cluster = Read-Host -Prompt 'Input the name of the Cluster'
+﻿$Cluster1 = Read-Host -Prompt 'Input the name of the Cluster'
 $FileLocation = "C:\temp\VM_CLuster_Host_Datastore_GHQ.csv"
 
-Get-VM -Location $Cluster | Select Name, @{N="Cluster";E={Get-Cluster -VM $_}}, `
+Get-VM -Location $Cluster1 | Select Name, @{N="Cluster";E={Get-Cluster -VM $_}}, `
 @{N="ESX Host";E={Get-VMHost -VM $_}}, `
 @{N="Datastore";E={Get-Datastore -VM $_}} | Sort Name | Export-Csv -NoTypeInformation $FileLocation
